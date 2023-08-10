fb74d6edf3318e3f59e960f4a94cd1b587bd6a72
Kyle Ryu <kyleryu6698@gmail.com>
2023-08-10 09:52:33 +0900

    페이지 정의 및 라우팅 정리

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  <h1>404 페이지</h1>
+  <div>페이지를 찾을 수 없습니다.</div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
