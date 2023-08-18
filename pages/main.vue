f1ffba8136690175b6c1c9b9429d64786b52fb7b
Kyle Ryu <kyleryu6698@gmail.com>
2023-08-18 22:40:44 +0900

    axios 설치 및 API 호출

--- pages/main.vue
+++ pages/main.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <p>메인 페이지입니다.</p>
-  </div>  
+    <p>메인 페이지</p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  async created() {
+    const response = await axios.get('https://localhost:3000/products')
+    console.log(response)
+  }
 }
 </script>
+
+
+<style>
+</style>
